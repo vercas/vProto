1fdd5f5d03934313efad3dc75fed95ded03b5711
Alexandru Maftei <maftei95@gmail.com>
2013-12-15 00:32:04 +0200

    Added binary serialization utility class
    + Added internal thread-safe utility class for binary serialization.

---
+++ vProto/Internal Utilities/Binary Serialization.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace vProto.Internal_Utilities
+{
+    internal static class BinarySerialization
+    {
+        [ThreadStatic]
+        private static BinaryFormatter formatter;
+
+        [ThreadStatic]
+        private static MemoryStream buffer;
+
+        public static byte[] Serialize(object o)
+        {
+            if (formatter == null)
+            {
+                formatter = new BinaryFormatter();
+                buffer = new MemoryStream(4096);
+            }
+
+            buffer.Seek(0, SeekOrigin.Begin);
+            buffer.SetLength(0L);
+
+            formatter.Serialize(buffer, o);
+
+            return buffer.ToArray();
+        }
+    }
+}
