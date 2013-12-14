9318288df0ac9059924dd4d30c0792ff0f401738
Alexandru Maftei <maftei95@gmail.com>
2013-12-15 00:17:38 +0200

    Changed bit conversion code
    > Bit conversion code now uses per-thread static buffers instead of
    locking on a buffer.
    
    The code is yet unused, though.

--- vProto/Internal Utilities/Bit Conversions.cs
+++ vProto/Internal Utilities/Bit Conversions.cs
@@ -2,20 +2,20 @@
 
 namespace vProto.Internal_Utilities
 {
-    internal static class Bit_Conversions
+    internal static class BitConversion
     {
-        static object buffLock = new object();
-        static byte[] buff = new byte[16];
+        [ThreadStatic]
+        static byte[] buff;
 
         public static int CombineShorts(short a, short b)
         {
-            lock (buffLock) //  BLOODY MULTITHREADING M8
-            {
-                BitConverter.GetBytes(a).CopyTo(buff, 0);
-                BitConverter.GetBytes(b).CopyTo(buff, 2);
+            if (buff == null)
+                buff = new byte[16];
 
-                return BitConverter.ToInt32(buff, 0);
-            }
+            BitConverter.GetBytes(a).CopyTo(buff, 0);
+            BitConverter.GetBytes(b).CopyTo(buff, 2);
+
+            return BitConverter.ToInt32(buff, 0);
         }
     }
 }
