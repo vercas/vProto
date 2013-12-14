c003c4aad39e9e02189e8a4887b2857ca5292557
Alexandru Maftei <maftei95@gmail.com>
2013-12-15 00:46:01 +0200

    Improved binary serialization class
    + Added methods for deserializing from a byte array;
    > Now only seeks to origin of stream and nulls the length if the stream
    already existed.

--- vProto/Internal Utilities/Binary Serialization.cs
+++ vProto/Internal Utilities/Binary Serialization.cs
@@ -12,6 +12,8 @@ namespace vProto.Internal_Utilities
         [ThreadStatic]
         private static MemoryStream buffer;
 
+
+
         public static byte[] Serialize(object o)
         {
             if (formatter == null)
@@ -19,13 +21,44 @@ namespace vProto.Internal_Utilities
                 formatter = new BinaryFormatter();
                 buffer = new MemoryStream(4096);
             }
-
-            buffer.Seek(0, SeekOrigin.Begin);
-            buffer.SetLength(0L);
+            else
+            {
+                buffer.Seek(0, SeekOrigin.Begin);
+                buffer.SetLength(0L);
+            }
 
             formatter.Serialize(buffer, o);
 
             return buffer.ToArray();
+        }
+
+
+
+        public static object Deserialize(byte[] data)
+        {
+            if (formatter == null)
+            {
+                formatter = new BinaryFormatter();
+                buffer = new MemoryStream(4096);
+            }
+            else
+            {
+                buffer.Seek(0, SeekOrigin.Begin);
+                buffer.SetLength(0L);
+            }
+
+            buffer.Write(data, 0, data.Length);
+
+            buffer.Seek(0, SeekOrigin.Begin);
+
+            return formatter.Deserialize(buffer);
+        }
+
+
+
+        public static TResult Deserialize<TResult>(byte[] data)
+        {
+            return (TResult)Deserialize(data);  //  BASICALLY, sugar syntax for myself. :(
         }
     }
 }
